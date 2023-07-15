c7b90309c5afa1c4dde78b77112db466da0efa93
Игорь Зацарин <dreadnought26@gmail.com>
2023-07-15 17:11:13 +0300

    Update compose file.

--- google_calendar.py
+++ google_calendar.py
@@ -17,17 +17,19 @@ class GoogleCalendar:
 
     def _get_creds(self):
         creds = None
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        if os.path.exists('creds/token.json'):
+            creds = Credentials.from_authorized_user_file(
+                'creds/token.json', SCOPES
+            )
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES
+                    'creds/credentials.json', SCOPES
                 )
                 creds = flow.run_local_server(port=0)
-            with open('token.json', 'w') as token:
+            with open('creds/token.json', 'w') as token:
                 token.write(creds.to_json())
         return creds
 
